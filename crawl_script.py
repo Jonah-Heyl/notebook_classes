06d77dbef0821a2611bcb147706378c12b3053a2
Jonah Heyl <97567639+Jonah-Heyl@users.noreply.github.com>
2022-06-28 09:56:30 -0700

    Update crawl_script.py

--- crawl_script.py
+++ crawl_script.py
@@ -1,4 +1,4 @@
-
+#here we are just importing some modules we need
 from sys import argv
 import os
 
@@ -12,36 +12,38 @@ try:
 except:
     os.system('pip install re')
     import re 
-
+    
+#this is the default global marker
 m="#!Z!s"
 
+#this is checks the first line in each cell to see if it has the marker
 def check(cell):
-    global m
-    try:
-        content = cell['source'][0]
+    global m # we pass m like this so the function only has one arg, means we can use the filter function
+    try: 
+        content = cell['source'][0] 
     except:
         return False
-    pattern= re.compile(m)
+    pattern= re.compile(m) #this uses regex to check if the marker is in the first line
     q = pattern.search(content)
-    if ( q is  None):
+    if ( q is  None): #if the marker is not in the first row we keep the cell
         return True
     return False
 
 
-def create_question(file,new_file):
+def create_question(file,new_file): #this function creates the question files
     marker="#!ANS!"
     global m
     m =marker
 
-    with open(file,"r") as f:
+    with open(file,"r") as f: #here we open the notebook as a json
         t=json.load(f)
     notebook=t['cells']
-    keep= list(filter(check,notebook))
+    keep= list(filter(check,notebook)) #this returns all the cells we want to keep
     t['cells']=keep
-    with open (new_file,"w") as l:
+    with open (new_file,"w") as l: #this makes the new json file
         json.dump(t,l)
 
-
+#this code is used to walk through all the files in a dirrectory and apply the code if nessary
 for root, subFolders, files in os.walk(argv[1]):
     for fname in files:
         if fname.endswith(".ipynb") and not fname.endswith("_QU.ipynb"):
